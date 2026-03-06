52e4715b226dcaf9c4267e9690c75400865e6589
mendozajoarnnieangeles-hash <mendozajoarnnieangeles@gmail.com>
2026-03-06 20:47:29 +0800

    Update content.js

--- content.js
+++ content.js
@@ -3,24 +3,27 @@ link.rel = "stylesheet";
 link.href = chrome.runtime.getURL("style.css");
 document.head.appendChild(link);
 
-document.addEventListener("DOMContentLoaded", () => {
-    let targetInput = document.getElementById("reject_reason");
-    console.log(targetInput); 
-
-    // let targetInput = document.getElementById("addRemarkInput");
+let isBannerActive = false;
 
 let remarks_array = ["God", "Jesus", "Allah", "Buddha", "Hindu Deity", "angel", "witch", "cross", "pentagram", "mosque", "quran", "cleavage", "close-up", "45 degrees", "less than 45 degrees", "suspected naked", "exaggerated breast", "kissing"];
 
-targetInput.addEventListener("mousedown", (event) => {
-    if (event.button === 1) {
+let body = document.body;
+
+body.addEventListener("mousedown", (event) => {
+ 
+    if (event.button === 1 && !isBannerActive) {
+        console.log("mousedown event is detected!");
+        
+        isBannerActive = true;
         event.preventDefault();
 
-        // create a banner
+        let targetInput = document.getElementById("reject_reason");
+        console.log(targetInput);
 
         const banner = document.createElement("div");
         banner.id = "remarker-banner";
 
-        // generate buttons 
+        // generate buttons
 
         remarks_array.forEach(element => {
 
@@ -28,26 +31,42 @@ targetInput.addEventListener("mousedown", (event) => {
             btn.textContent = element
             banner.appendChild(btn)
 
-        });
+        })
 
-         // assigning event listener for the whole div
+        // generating close button
 
-            banner.addEventListener("click", function (event) {
+        let closeBtn = document.createElement("button");
+        closeBtn.id = "bannerCloseBtn";
+        closeBtn.textContent = "close";
+        banner.appendChild(closeBtn);
 
+        // assigning event listener for the whole div
+
+        banner.addEventListener("click", function (event) {
+
+            if (event.target.tagName !== "BUTTON") {
+                isBannerActive = false;
+                return;
+
+            }
+            else if (event.target.id === "bannerCloseBtn") {
+                isBannerActive = false;
+                banner.remove();
+                return;
+
+            }
+            else {
+                isBannerActive = false;
                 targetInput.value += `\n ${event.target.textContent}`;
                 banner.remove();
 
-            })
+            }
+
+        })
 
         document.body.appendChild(banner)
 
     }
-});
-    
-});
 
-
-
-
-
+}, true);
 
